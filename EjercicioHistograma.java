11f6d4926deadd2a0e1f3657ac07fff150ad9d43
unknown <jorgesant.fernandez@gmail.com>
2023-07-07 12:24:27 +0200

    Primer commit

--- EjercicioHistograma.java
+++ EjercicioHistograma.java
@@ -2,12 +2,12 @@ package Seccion12;
 
 import java.util.Scanner;
 
-public class Histograma {
+public class EjercicioHistograma {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] arreglo = new int[12];
 
-        
+        // ingreamos los datos por ejemplo {4, 3, 4, 6, 6, 4, 1, 4, 5, 4, 1, 1}
         for (int i = 0; i < arreglo.length; i++) {
             System.out.println("Ingrese un numero de 1 a 6: ");
             arreglo[i] = scanner.nextInt();
